c0b5c2ac7702ff3b2ed78e492ca892184ff137ff
forsrc <forsrc@gmail.com>
2018-07-12 16:46:25 +0900

    Update GoogleCalendarController.java

--- src/main/java/com/forsrc/google/calendar/GoogleCalendarController.java
+++ src/main/java/com/forsrc/google/calendar/GoogleCalendarController.java
@@ -2,6 +2,8 @@ package com.forsrc.google.calendar;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -56,6 +60,31 @@ public class GoogleCalendarController {
             list.add(String.format("%s -> %s", event.getSummary(), start));
         }
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/list/{email}")
+    public ResponseEntity<List<Event>> listeEmail(@PathVariable String email, //
+            @RequestParam(name = "min", required = false) String min, //
+            @RequestParam(name = "max", required = false) String max) throws GeneralSecurityException, IOException,
+            InterruptedException, ExecutionException, TimeoutException, ParseException {
+
+        List<String> list = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        DateTime minDateTime = min == null ? new DateTime(new Date()) : new DateTime(sdf.parse(min));
+        DateTime maxDateTime = max == null ? null : new DateTime(sdf.parse(max));
+
+        Calendar calendar = googleTools.getCalendar();
+        List<Event> items = googleTools.list(calendar, email, minDateTime, maxDateTime);
+        for (Event event : items) {
+            DateTime start = event.getStart().getDateTime();
+            if (start == null) {
+                start = event.getStart().getDate();
+            }
+            System.out.printf("%s (%s)\n", event.getSummary(), start);
+            list.add(String.format("%s -> %s", event.getSummary(), start));
+        }
+        return new ResponseEntity<>(items, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/stop")
